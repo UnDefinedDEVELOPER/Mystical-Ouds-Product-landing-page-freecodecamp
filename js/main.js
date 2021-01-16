02794c00179d781c9f797beae69c5aaec0f5e289
UnDefinedDEVELOPER <muhammedhalilovic123@gmail.com>
2021-01-16 11:43:49 +0100

    just phasing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navBar = document.querySelector('.nav');
+const title = document.querySelector('.title');
+let width = window.innerWidth;
+
+function query(x) {
+    if (x.matches) { // If media query matches
+      hamburgerMenu.classList.remove('hide');
+      hamburgerMenu.classList.add('flex');
+      navBar.classList.add('hide');
+    } else {
+        hamburgerMenu.classList.add('hide');
+        hamburgerMenu.classList.remove('flex');
+        navBar.classList.remove('hide');
+    }
+  }
+  
+  const x = window.matchMedia("(max-width: 1000px)");
+  query(x); // Call listener function at run time
+  x.addListener(query); // Attach listener function on state changes */ 
 
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('showx');
+    title.classList.toggle('hide');
+    navBar.classList.toggle('hide');
 })
